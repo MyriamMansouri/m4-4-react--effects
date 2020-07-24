04b24c8f1ce4f55e9b81e851db3341f85a1f0d45
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-23 23:34:04 -0400

    Exercise 1: Finishing the UI

---
+++ src/components/Item.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const ItemWrapper = styled.button`
+  background-color: inherit;
+  color: inherit;
+  text-align: inherit;
+  font-size: inherit;
+  border: none;
+  border-bottom: grey 1px solid;
+  padding:10px 0;
+  &:focus {
+      outline:blue solid 3px
+  }
+`;
+
+const Item = ({ name, cost, value }) => {
+  return (
+    <ItemWrapper>
+      <h4>{name}</h4>
+      <p>
+        Cost: {cost} cookies. Produces {value} cookies/second.
+      </p>
+    </ItemWrapper>
+  );
+};
+
+export default Item;
